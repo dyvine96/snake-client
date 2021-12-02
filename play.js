c40e8a6daa1496fa4a4eddb8b45e4d95a527a5ed
Dymika Harte <dymika@unsgnd.ca>
2021-12-02 11:55:11 +0000

    stin setup

--- play.js
+++ play.js
@@ -1,8 +1,29 @@
 const net = require("net");
 const connect = require('./client');
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect.connect();
 
-console.log(connect);
+setupInput();
 
+//console.log(connect);
+
+
+// // setup interface to handle user input from stdin
+// //program should exit on ctrl+c
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// };
+
+// stdin.on("data", handleUserInput) => {
+//   const handleUserInput = function () {
+//     if (key === '\u0003') {
+//       process.exit();
+//     }
+//   };
+// };
